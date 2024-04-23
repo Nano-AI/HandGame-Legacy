a5c565f44ba56bbf330e5309e1ef824c48f28523
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2024-04-23 07:07:07 -0700

    A

--- game/scripts/HandTracking.cs
+++ game/scripts/HandTracking.cs
@@ -19,9 +19,18 @@ public partial class HandTracking : Node {
 	private float calibratedScale = 1;
 
 	private Skeleton3D skel;
+
+	private MeshInstance3D meshInstance3D;
+	private ArrayMesh mesh;
+
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+		mesh = new ArrayMesh();
+		meshInstance3D = new MeshInstance3D();
+		AddChild(meshInstance3D);
+		meshInstance3D.Mesh = mesh;
+		
 		GD.Print("Setting up hand tracking...");
 		handPoints = new Array<Node3D>();
 		handPositions = new Array<Vector3>();
@@ -53,61 +62,28 @@ public partial class HandTracking : Node {
 		// GD.Print(data);
 		string[] points = data.Split(',');
 		for (int i = 0; i < 21; i++) {
-			float x = float.Parse(points[i * 3]) / 100;
-			float y = float.Parse(points[i * 3 + 1]) / 100;
-			float z = float.Parse(points[i * 3 + 2]) / 100;
+			float x = float.Parse(points[i * 3]) / 70;
+			float y = float.Parse(points[i * 3 + 1]) / 70;
+			float z = float.Parse(points[i * 3 + 2]) / 70;
 			Vector3 newPoint = new Vector3(-1 * x, y, z);	
 			
 			Node3D point = (Node3D) handPoints[i];
 
 			float distance = handPositions[9].DistanceTo(handPositions[0]);
-			// Vector3 zOffset = new Vector3(0, 0, (distance / calibratedScale) * 10);
 			Vector3 zOffset = new Vector3(0, 0, (distance / calibratedScale) * 10);
 			
 			if (newPoint.DistanceTo(handPositions[i]) > minDistanceAway) {
 				handPositions[i] = new Vector3(-1 * x, y, z);
 			}
 
-			// point.Position = handPositions[i] - offset - zOffset;
-			point.Position = handPositions[i] - offset;
+			point.Position = handPositions[i] - offset - zOffset;
 			
 			UpdateHand();
 		}
 	}
 
 	public void UpdateHand() {
-		GD.Print(skel.GetBoneCount(), handPositions.Count);
-		for (int i = 0; i < handPositions.Count; i++) {
-			if (i % 4 == 0) continue;
-			Vector3 at = handPoints[i].Position;
-			Vector3 next = handPoints[i + 1].Position;
-			Quaternion q;
-			Vector3 a = at.Cross(next);
-			q.X = a.X;
-			q.Y = a.Y;
-			q.Z = a.Z;
-			q.W = (float) Math.Sqrt(
-				Math.Pow(at.Length(), 2) * Math.Pow(at.Length(), 2) + at.Dot(next)
-				);
 
-			Vector3 direction = at - next;
-			Quaternion rotation = Quaternion.Identity;
-			rotation.X = Mathf.Atan2(-direction.Z, direction.Y);
-			rotation.Y = Mathf.Atan2(direction.X, Mathf.Sqrt(direction.Y * direction.Y + direction.Z * direction.Z));
-			rotation.Z = 0;
-
-			Basis basis = new Basis(q);
-			handPoints[i].Transform = new Transform3D(basis, handPoints[i].Transform.Origin);
-
-			// skel.SetBonePosePosition(bone, handPoints[i].GlobalPosition);
-		}
-	}
-
-	private Quaternion CalculateRotation(Vector3 fromDirection, Vector3 toDirection)
-	{
-		Vector3 axis = fromDirection.Cross(toDirection).Normalized();
-		float angle = Mathf.Acos(fromDirection.Dot(toDirection));
-		return new Quaternion(axis, angle);
 	}
 
 	public void Calibrate() {
@@ -115,23 +91,5 @@ public partial class HandTracking : Node {
 		offset = center;
 
 		calibratedScale = handPositions[9].DistanceTo(handPositions[0]);
-	}
-
-	private void DrawDebugSphere(Vector3 pos, float rad) {
-		var sceneRoot = GetTree().Root.GetChildren()[0];
-		var sphere = new SphereMesh();
-		sphere.RadialSegments = 4;
-		sphere.Rings = 4;
-		sphere.Radius = rad;
-		sphere.Height = rad * 2;
-
-		var material = new StandardMaterial3D();
-		material.AlbedoColor = new Color(0, 1, 0);
-		
-		sphere.SurfaceSetMaterial(0, material);
-		var node = new MeshInstance3D();
-		node.MaterialOverride = material;
-		node.Position = pos;
-		sceneRoot.AddChild(node);
 	}
 }
